cdb7fb15353278ed9f427ed3bd30b1144e2a02b8
Tati76 <vincentfossum@hotmail.com>
2016-06-13 11:26:12 +0200

    added a Settings.json in order to replace languages.json and home code changed to work on the new json

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -1,4 +1,36 @@
 Home = React.createClass({  
+
+	getInitialState: function() {
+		return{
+			usedLang : require("./languages/Settings.json").setups[this.props.index].language, // No the default language
+			usedLangObject : require("./languages/Settings.json").setups[this.props.index].pageHome, // refers to the object in fuction of the language selected 
+			}
+	},
+
+
+
+	componentWillReceiveProps: function(nextProps) {
+		this.setState({
+			usedLang : require("./languages/Settings.json").setups[nextProps.index].language, // No the default language
+			usedLangObject : require("./languages/Settings.json").setups[nextProps.index].pageHome, // refers to the object in fuction of the language selected 
+			});	},
+
+	shouldComponentUpdate: function(nextProps, nextState) {
+		if (nextProps.language !== this.props.language) // if a prop change, update !
+		{
+			return nextProps.language !== this.props.language;
+		}
+		else if (nextProps.index !== this.props.index)
+		{
+			return nextProps.index !== this.props.index;
+		}
+		else
+		{
+			return false;
+		}
+	},
+
+
   render() {
 	  
 	  var tempCurrentRouteArray = FlowRouter.current().path.split("/");
@@ -24,10 +56,10 @@ Home = React.createClass({
 
 		<div className="btn-group btn-group-justified" role="group" aria-label="...">
 			  <div className="btn-group" role="group">
-			    <a href={res}><button type="button" className="btn btn-default">Lagre</button></a>
+			    <a href={res}><button type="button" className="btn btn-default">{this.state.usedLangObject.buttons.save}</button></a>
 			  </div>
 			  <div className="btn-group" role="group">
-			   <a href="#"> <button type="button" className="btn btn-default">Search</button></a>
+			   <a href="#"> <button type="button" className="btn btn-default">{this.state.usedLangObject.buttons.search}</button></a>
 			  </div>
 		</div>
 
